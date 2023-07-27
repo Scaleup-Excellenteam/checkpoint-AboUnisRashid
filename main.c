631f1359106c6516ef9680f577aa0da531b1935f
AboUnisRashid <93597900+AboUnisRashid@users.noreply.github.com>
2023-07-27 16:41:08 +0300

    seventh commit

--- main.c
+++ main.c
@@ -127,6 +127,14 @@ void INITDB() {
 }
 //-----------------------------------FUNCTION-----------------------------------
 void add_new_student(char* first_name, char* last_name, char* phone, int level, int class, int* grades) {
+
+    // CHECK IF ARGUMENTS ARE VALID
+    if (!(level >= 0 && level < NUM_LEVELS && class >= 0 && class < NUM_CLASSES))
+    {
+        printf("LEVEL/CLASS ARGUMENTS ARE NOT VALID");
+        return;
+    }
+
     struct Student* new_student = (struct Student*) malloc(sizeof (struct Student));
     // IN CASE ALLOCATION OF STUDENT SUCCEEDED
     if (new_student != NULL) {
@@ -154,30 +162,35 @@ void add_new_student(char* first_name, char* last_name, char* phone, int level, 
 }
 //-----------------------------------FUNCTION-----------------------------------
 void delete_student(char* first_name, char* last_name, int level, int class) {
-    // CHECK IF ARGUMENTS ARE VALID
-    if (!(level >= 0 && level < NUM_LEVELS && class >= 0 && class < NUM_CLASSES))
+    // Check if arguments are valid
+    if (!(level >= 0 && level < NUM_LEVELS && class >= 0 && class < NUM_CLASSES)) {
         printf("LEVEL/CLASS ARGUMENTS ARE NOT VALID");
+        return;
+    }
 
-    struct Student* curr = s.DB[level-1][class-1];
+    struct Student* curr = s.DB[level - 1][class - 1];
     struct Student* prev = NULL;
-    // FIND THE STUDENT LOCATION
-    while(curr != NULL) {
-        if(strcmp(curr->first_name, first_name) == 0 && strcmp(curr->last_name, last_name) == 0) {
+
+    // Find the student location
+    while (curr != NULL) {
+        if (strcmp(curr->first_name, first_name) == 0 && strcmp(curr->last_name, last_name) == 0) {
             if (prev == NULL) {
-                s.DB[level][class] = curr->next;
+                s.DB[level - 1][class - 1] = curr->next;
             } else {
                 prev->next = curr->next;
             }
             free(curr);
-            printf("Student %s %s removed from Level %d, Class %d.\n", first_name, last_name, level + 1, class + 1);
+            printf("Student %s %s removed from Level %d, Class %d.\n", first_name, last_name, level, class);
             printf("-------------------------------\n");
             return;
         }
         prev = curr;
         curr = curr->next;
     }
-    printf("Student %s %s not found in Level %d, Class %d.\n", first_name, last_name, level + 1, class + 1);
+
+    printf("Student %s %s not found in Level %d, Class %d.\n", first_name, last_name, level, class);
 }
+
 //-----------------------------------FUNCTION-----------------------------------
 struct Student* get_student(char* first_name, char* last_name, int level, int class) {
     if (level >= 0 && level < NUM_LEVELS && class >= 0 && class < NUM_CLASSES) {
@@ -286,23 +299,175 @@ void top_students_in_course(char course) {
         printf("-------------------------------\n");
     }
 }
+//-----------------------------------FUNCTION-----------------------------------
+void candidates_for_leaving(int max_grades_sum) {
+    printf("Students Candidates for Leaving:\n");
+    for (int level = 0; level < NUM_LEVELS; ++level) {
+        for (int class = 0; class < NUM_CLASSES; ++class) {
+            struct Student* current = s.DB[level][class];
+            while (current != NULL) {
+                int grades_sum = 0;
+                for (int i = 0; i < NUM_COURSES; i++) {
+                    grades_sum += current->course[i].grade;
+                }
 
+                if (grades_sum <= max_grades_sum) {
+                    printf("Student Name: %s %s, Level: %d, Class: %d\n", current->first_name, current->last_name, level + 1, class + 1);
+                }
+                current = current->next;
+            }
+        }
+    }
+    printf("-------------------------------\n");
+}
+//-----------------------------------FUNCTION-----------------------------------
+void average_grade_for_course_in_level(char course, int level) {
+    printf("Average Grade for Course %c in Level %d:\n", course, level);
 
+    if (level < 1 || level > NUM_LEVELS) {
+        printf("Invalid level!\n");
+        return;
+    }
 
+    int total_grades = 0;
+    int num_students = 0;
+
+    // Calculate total grades and number of students for the given course in the specified level
+    for (int class = 0; class < NUM_CLASSES; ++class) {
+        struct Student* current = s.DB[level - 1][class];
+        while (current != NULL) {
+            for (int i = 0; i < NUM_COURSES; ++i) {
+                if (current->course[i].c_name[0] == course) {
+                    total_grades += current->course[i].grade;
+                    num_students++;
+                    break; // Exit the inner loop since we found the course in the current student
+                }
+            }
+            current = current->next;
+        }
+    }
+
+    if (num_students > 0) {
+        double average_grade = (double)total_grades / num_students;
+        printf("Average Grade: %.2f\n", average_grade);
+    } else {
+        printf("No students enrolled in Course %c in Level %d.\n", course, level);
+    }
+}
+//-----------------------------------FUNCTION-----------------------------------
+void displayMainMenu() {
+    printf("\nSelect an option:\n");
+    printf("1. Add a new student\n");
+    printf("2. Edit student grade\n");
+    printf("3. Print data for a cell (level, class)\n");
+    printf("4. Search for a student\n");
+    printf("5. Top students in a course\n");
+    printf("6. Candidates for leaving\n");
+    printf("7. Average grade for a course in a level\n");
+    printf("8. Delete a student\n"); // New option for deleting a student
+    printf("0. Exit\n");
+    printf("Enter your choice: ");
+}
+
+//-----------------------------------FUNCTION-----------------------------------
+void processMenuChoice(int choice) {
+    char first_name[NAME_LENGTH], last_name[NAME_LENGTH], phone[PHONE_NUMBER];
+    int level, class, grades[NUM_OF_GRADES], grade;
+    char course;
+    int max_grades_sum;
+
+    switch (choice) {
+        case 1:
+            printf("Enter first name: ");
+            scanf("%s", first_name);
+            printf("Enter last name: ");
+            scanf("%s", last_name);
+            printf("Enter phone number: ");
+            scanf("%s", phone);
+            printf("Enter level: ");
+            scanf("%d", &level);
+            printf("Enter class: ");
+            scanf("%d", &class);
+            printf("Enter grades (10 grades separated by spaces): ");
+            for (int i = 0; i < NUM_OF_GRADES; i++) {
+                scanf("%d", &grades[i]);
+            }
+            add_new_student(first_name, last_name, phone, level, class, grades);
+            break;
+        case 2:
+            printf("Enter first name: ");
+            scanf("%s", first_name);
+            printf("Enter last name: ");
+            scanf("%s", last_name);
+            printf("Enter level: ");
+            scanf("%d", &level);
+            printf("Enter class: ");
+            scanf("%d", &class);
+            printf("Enter course name (single character): ");
+            scanf(" %c", &course);
+            printf("Enter new grade: ");
+            scanf("%d", &grade);
+            edit_student_grade(first_name, last_name, level, class, course, grade);
+            break;
+        case 3:
+            printf("Enter level: ");
+            scanf("%d", &level);
+            printf("Enter class: ");
+            scanf("%d", &class);
+            print_data_for_cell(level, class);
+            break;
+        case 4:
+            printf("Enter first name: ");
+            scanf("%s", first_name);
+            printf("Enter last name: ");
+            scanf("%s", last_name);
+            search_student(first_name, last_name);
+            break;
+        case 5:
+            printf("Enter course name (single character): ");
+            scanf(" %c", &course);
+            top_students_in_course(course);
+            break;
+        case 6:
+            printf("Enter maximum sum of grades: ");
+            scanf("%d", &max_grades_sum);
+            candidates_for_leaving(max_grades_sum);
+            break;
+        case 7:
+            printf("Enter course name (single character): ");
+            scanf(" %c", &course);
+            printf("Enter level: ");
+            scanf("%d", &level);
+            average_grade_for_course_in_level(course, level);
+            break;
+        case 8: // New case for deleting a student
+            printf("Enter first name: ");
+            scanf("%s", first_name);
+            printf("Enter last name: ");
+            scanf("%s", last_name);
+            printf("Enter level: ");
+            scanf("%d", &level);
+            printf("Enter class: ");
+            scanf("%d", &class);
+            delete_student(first_name, last_name, level, class);
+            break;
+        case 0:
+            printf("Exiting...\n");
+            break;
+        default:
+            printf("Invalid choice. Please try again.\n");
+    }
+}
 //-----------------------------------MAIN-----------------------------------
 int main() {
     INITDB();
 
-    // Add a new student and link courses
-    int new_grades[NUM_OF_GRADES] = {80, 85, 90, 77, 95, 88, 93, 82, 91, 89};
-    add_new_student("John", "Doe", "123456789", 3, 4, new_grades);
+    int choice;
+    do {
+        displayMainMenu();
+        scanf("%d", &choice);
+        processMenuChoice(choice);
+    } while (choice != 0);
 
-    // Edit student grade
-    edit_student_grade("John", "Doe", 3 ,4, 'A', 0);
-
-    // Print data after adding the new student
-    //print_data_for_cell(3, 4); // Level 3, Class 4 (updated)
-    //search_student("John", "Doe");
-    top_students_in_course('A');
     return 0;
 }
